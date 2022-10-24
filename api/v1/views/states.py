84eabdf86cfe379e66f9872e1b1446c23cc38da3
Yordi-Dereje <yordadereje@gmail.com>
2022-10-25 01:33:37 +0300

    Api 4.6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,8 @@ module to generate json response
 """
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response, abort, request
+from api.vi import app
 from models import storage
 from models.base_model import BaseModel, Base
 from models.state import State
@@ -25,21 +26,24 @@ def state_by_id(state_id):
     """ address to display state by id """
     response = storage.get(State, state_id)
     if response is None:
-        abort(404)
+        app.abort(404)
     return jsoniify(response.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                strict_slashes=False)
 def del_state(state_id=None):
     """ delete state by id """
     if state_id is None:
-        abort(404)
+        app.abort(404)
     else:
         trash = storage.get(State, state_id)
-        if response is not None:
+        if trash is not None:
             storage.delete(trash)
             storage.save()
             return make_response(jsonify({}), 200)
+        else:
+            app.abort(404)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
