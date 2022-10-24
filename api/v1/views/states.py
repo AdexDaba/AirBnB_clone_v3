4d1c243468eed2fb5760deb50103ed85675ee31a
Yordi-Dereje <yordadereje@gmail.com>
2022-10-25 01:42:25 +0300

    Api 4.75

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,16 +4,14 @@ module to generate json response
 """
 
 from api.v1.views import app_views
-from flask import jsonify, make_response, abort, request
-from api.vi import app
+from flask import jsonify, make_response, request, abort
 from models import storage
-from models.base_model import BaseModel, Base
 from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False)
 def all_states():
-    """ address to display all states """
+    """ display all states """
     response = []
     states = storage.all(State)
     for state in states.values():
@@ -23,19 +21,19 @@ def all_states():
 
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def state_by_id(state_id):
-    """ address to display state by id """
+    """ display state by id """
     response = storage.get(State, state_id)
     if response is None:
-        app.abort(404)
-    return jsoniify(response.to_dict())
+        abort(404)
+    return jsonify(response.to_dict())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def del_state(state_id=None):
     """ delete state by id """
     if state_id is None:
-        app.abort(404)
+        abort(404)
     else:
         trash = storage.get(State, state_id)
         if trash is not None:
@@ -43,17 +41,20 @@ def del_state(state_id=None):
             storage.save()
             return make_response(jsonify({}), 200)
         else:
-            app.abort(404)
+            abort(404)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """ creates a new state """
-    new = request.get_json()
+    try:
+        new = request.get_json()
+    except Exception:
+        pass
     if new is None or type(new) is not dict:
-        abort(400, description='Not a JSON')
+        abort(400, 'Not a JSON')
     if 'name' not in new.keys():
-        abort(400, description='Missing name')
+        abort(400, 'Missing Name')
     response = State(**new)
     response.save()
     return make_response(jsonify(response.to_dict()), 201)
@@ -65,9 +66,12 @@ def update_state(state_id=None):
     response = storage.get(State, state_id)
     if state_id is None or response is None:
         abort(404)
-    new = request.get_json()
+    try:
+        new = request.get_json()
+    except Exception:
+        pass
     if new is None or type(new) is not dict:
-        abort(400, description='Not a JSON')
+        abort(400, 'Not a JSON')
     for key in new.keys():
         if key != 'id' and key != 'created_at' and key != 'updated_at':
             setattr(response, key, new[key])
